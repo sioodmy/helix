c99fd6c1022462fd9803f0c3ee3325427c3ff724
Sora <dream@neoncity.dev>
2023-03-18 18:11:08 +0000

    Add C/C++ Queries

---
+++ runtime/queries/c/context.scm
@@ -0,0 +1,27 @@
+; Credits to nvim-treesitter/nvim-treesitter-context
+(preproc_if
+) @context
+
+(preproc_ifdef
+) @context
+
+(function_definition
+) @context
+
+(for_statement
+) @context
+
+(if_statement
+) @context
+
+(while_statement
+) @context
+
+(do_statement
+) @context
+
+(struct_specifier
+) @context
+
+(enum_specifier
+) @context
