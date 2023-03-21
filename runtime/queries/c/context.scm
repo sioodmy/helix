4fdac88d6ca3269b98472d059f022033c333e75c
SoraTenshi <dream@neoncity.dev>
2023-03-21 21:03:29 +0100

    Fix C/C++ Queries to include @context.end

--- runtime/queries/c/context.scm
+++ runtime/queries/c/context.scm
@@ -1,27 +1,17 @@
 ; Credits to nvim-treesitter/nvim-treesitter-context
-(preproc_if
-) @context
-
-(preproc_ifdef
-) @context
 
 (function_definition
+  body: (_) @context.end
 ) @context
 
-(for_statement
-) @context
-
-(if_statement
-) @context
-
-(while_statement
-) @context
-
-(do_statement
-) @context
-
-(struct_specifier
-) @context
-
-(enum_specifier
-) @context
+[
+  (preproc_if)
+  (preproc_ifdef)
+  (function_definition)
+  (for_statement)
+  (if_statement)
+  (while_statement)
+  (do_statement)
+  (struct_specifier)
+  (enum_specifier)
+] @context
