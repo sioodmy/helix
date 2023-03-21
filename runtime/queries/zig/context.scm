b563f21b3cf985feb303cb8ce24965603b99504e
SoraTenshi <dream@neoncity.dev>
2023-03-21 22:31:22 +0100

    Add Context queries for Zig

---
+++ runtime/queries/zig/context.scm
@@ -0,0 +1,19 @@
+
+(TopLevelDecl
+  (FnProto)
+  (Block) @context.end
+) @context
+
+[
+  (FnProto)
+  (ContainerDecl)
+  (ForStatement)
+  (IfStatement)
+  (InitList)
+  (LabeledStatement)
+  (LoopStatement)
+  (SwitchCase)
+  (SwitchProng)
+  (TestDecl)
+  (WhileStatement)
+] @context
