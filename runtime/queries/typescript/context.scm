0a3f40c410a4df070ae022394e18901fdc83437a
SoraTenshi <dream@neoncity.dev>
2023-03-22 19:41:13 +0100

    Add Typescript queries

---
+++ runtime/queries/typescript/context.scm
@@ -0,0 +1,30 @@
+(arrow_function
+	body: (_) @context.end
+) @context
+
+(function_declaration
+	body: (_) @context.end
+) @context
+
+(
+  (expression_statement
+  	(call_expression
+      	function: (_)))
+  (statement_block) @context.end
+) @context
+
+[
+  (call_expression)
+  (class_declaration)
+  (else_clause)
+  (for_statement)
+  (interface_declaration)
+  (interface_declaration)
+  (lexical_declaration)
+  (lexical_declaration)
+  (method_definition)
+  (object)
+  (pair)
+  (while_statement)
+] @context
+
