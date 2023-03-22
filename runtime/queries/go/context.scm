5d4a20ea16eca96793851dc180ede94d9d009d8e
SoraTenshi <dream@neoncity.dev>
2023-03-22 19:46:46 +0100

    Add Go queries

---
+++ runtime/queries/go/context.scm
@@ -0,0 +1,17 @@
+(function_declaration
+	result: (_) @context.end
+) @context
+
+(method_declaration
+	result: (_) @context.end
+) @context
+
+[
+	(function_declaration)
+	(method_declaration)
+  (const_declaration)
+  (if_statement)
+  (import_declaration)
+  (type_declaration)
+  (var_declaration)
+] @context
