c62a2d1e42b666177bf01f4257317d2fb955fa9b
SoraTenshi <dream@neoncity.dev>
2023-03-31 11:40:07 +0200

    Update context for go

--- runtime/queries/go/context.scm
+++ runtime/queries/go/context.scm
@@ -8,11 +8,14 @@
 	body: (_) @context.end
 ) @context
 
-([
+(for_statement
+	(_)) @context
+
+[
   (const_declaration)
 	(for_statement)
 	(if_statement)
   (import_declaration)
   (type_declaration)
   (var_declaration)
-] @context)
+] @context
